81e13e9dfd876117b9303ae09798153e1cff71fe
Eirik S. Morland <eirik@morland.no>
2014-10-09 17:03:46 +0200

    Add coverage and some badges.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 petulant-octo-ironman
 =====================
+[![Build Status](https://travis-ci.org/eiriksm/petulant-octo-ironman.svg?branch=master)](https://travis-ci.org/eiriksm/petulant-octo-ironman)
+[![Coverage Status](https://img.shields.io/coveralls/eiriksm/petulant-octo-ironman.svg)](https://coveralls.io/r/eiriksm/petulant-octo-ironman)
+
--- test/test.js
+++ test/test.js
@@ -2,8 +2,13 @@ require('should');
 
 var a = require('..')
 describe('Awesome module', function() {
-  it('Should return the expected value', function() {
+  it('Should return the expected value', function(done) {
     a(1.050, 1.010).toFixed(2).should.equal('5.24');
+    done();
+  });
+  it('Should complain', function(done) {
+    a.should.throw();
+    done();
   });
 });
 
